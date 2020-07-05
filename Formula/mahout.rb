7360d2eff0d4f460a064a9589c3a6b5fa69a36c6
SeekingMeaning <meaningseeking@protonmail.com>
2020-07-05 07:03:07 -0700

    Temporarily revert changes
    Let's see if this fixes CI

--- Formula/mahout.rb
+++ Formula/mahout.rb
@@ -1,31 +1,42 @@
 class Mahout < Formula
   desc "Library to help build scalable machine learning libraries"
   homepage "https://mahout.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=mahout/0.14.0/mahout-0.14.0-source-release.zip"
-  mirror "https://archive.apache.org/dist/mahout/0.14.0/mahout-0.14.0-source-release.zip"
-  sha256 "f95b257e3652ae9c6e6f649d154e7073cb554aa85bc996c9b483d796e2edab7d"
-  license "Apache-2.0"
-  head "https://github.com/apache/mahout.git"
+  url "https://www.apache.org/dyn/closer.lua?path=mahout/0.13.0/apache-mahout-distribution-0.13.0.tar.gz"
+  mirror "https://archive.apache.org/dist/mahout/0.13.0/apache-mahout-distribution-0.13.0.tar.gz"
+  sha256 "87bdc86e16b5817d6b5a810b94d7389604887f7de9c680f34faaf0cbb8dabf6f"
+  revision 1
 
-  depends_on "maven" => :build
+  head do
+    url "https://github.com/apache/mahout.git"
+    depends_on "maven" => :build
+  end
+
+  bottle :unneeded
+
   depends_on "hadoop"
-  depends_on :java => "1.8"
+  depends_on "openjdk"
 
   def install
-    ENV["JAVA_HOME"] = Language::Java.java_home("1.8")
+    ENV["JAVA_HOME"] = Formula["openjdk"].opt_prefix
 
-    chmod 755, "./bin"
-    system "mvn", "-DskipTests", "-DadditionalJOption=-Xdoclint:none", "clean", "install"
+    if build.head?
+      chmod 755, "./bin"
+      system "mvn", "-DskipTests", "clean", "install"
+    end
 
     libexec.install "bin"
 
-    libexec.install Dir["buildtools/target/*.jar"]
-    libexec.install Dir["core/target/*.jar"]
-    libexec.install Dir["examples/target/*.jar"]
-    libexec.install Dir["math/target/*.jar"]
+    if build.head?
+      libexec.install Dir["buildtools/target/*.jar"]
+      libexec.install Dir["core/target/*.jar"]
+      libexec.install Dir["examples/target/*.jar"]
+      libexec.install Dir["math/target/*.jar"]
+    else
+      libexec.install Dir["*.jar"]
+    end
 
     bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files libexec/"bin", Language::Java.java_home_env("1.8")
+    bin.env_script_all_files libexec/"bin", :JAVA_HOME => ENV["JAVA_HOME"]
   end
 
   test do
