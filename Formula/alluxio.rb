25b67d17bdb988ba380bfbb2b7c76a5f6addf5e6
Seeker <meaningseeking@protonmail.com>
2020-07-09 15:46:00 -0700

    alluxio 2.3.0

--- Formula/alluxio.rb
+++ Formula/alluxio.rb
@@ -1,10 +1,12 @@
 class Alluxio < Formula
   desc "Open Source Memory Speed Virtual Distributed Storage"
   homepage "https://www.alluxio.io/"
-  url "https://downloads.alluxio.io/downloads/files/1.8.2/alluxio-1.8.2-bin.tar.gz"
-  sha256 "e927f80aabf80ac0b47d4491a4320058bcd15f554fccec1375e8f6dcf243ebb4"
+  url "https://downloads.alluxio.io/downloads/files/2.3.0/alluxio-2.3.0-bin.tar.gz"
+  sha256 "7147c0957e6ea2c89d4b85867524ecaa5f038291f0f6be4a22320f4adae2253d"
 
   bottle :unneeded
+
+  patch :DATA
 
   def default_alluxio_conf
     <<~EOS
@@ -39,3 +41,22 @@ class Alluxio < Formula
     system bin/"alluxio", "version"
   end
 end
+
+__END__
+diff --git a/alluxio/v2.3.0/libexec/alluxio-config.sh b/alluxio/v2.3.0/libexec/alluxio-config.sh
+index 3569b96..612df11 100644
+--- a/alluxio/v2.3.0/libexec/alluxio-config.sh
++++ b/alluxio/v2.3.0/libexec/alluxio-config.sh
+@@ -51,12 +51,6 @@ if [[ -z "${JAVA}" ]]; then
+   fi
+ fi
+ 
+-# Check Java version == 1.8
+-JAVA_VERSION=$(${JAVA} -version 2>&1 | awk -F '"' '/version/ {print $2}')
+-if [[ $(echo "${JAVA_VERSION}" | awk -F. '{printf("%03d%03d",$1,$2);}') != 001008 ]]; then
+-  echo "Error: Alluxio requires Java 8, currently Java $JAVA_VERSION found."
+-  exit 1
+-fi
+ 
+ if [[ -n "${ALLUXIO_HOME}" ]]; then
+   ALLUXIO_JAVA_OPTS+=" -Dalluxio.home=${ALLUXIO_HOME}"
