ae9d09c132b05ff0d72f49d9ba825e2040b8d4f4
Seeker <meaningseeking@protonmail.com>
2020-07-11 14:46:28 -0700

    jdnssec-tools 0.16

--- Formula/jdnssec-tools.rb
+++ Formula/jdnssec-tools.rb
@@ -1,9 +1,8 @@
 class JdnssecTools < Formula
   desc "Java command-line tools for DNSSEC"
   homepage "https://github.com/dblacka/jdnssec-tools"
-  url "https://github.com/dblacka/jdnssec-tools/releases/download/v0.15/jdnssec-tools-0.15.tar.gz"
-  sha256 "1d4905652639b8b23084366eb2e2b33d5f534bf29fbf9b4becbf9e29f9b39fdf"
-  revision 1
+  url "https://github.com/dblacka/jdnssec-tools/archive/v0.16.tar.gz"
+  sha256 "0d5a7e97a30aa8ef9477e0cb9525a659d7653f8073b76a61f9cc7ffcaca2d25d"
   head "https://github.com/dblacka/jdnssec-tools.git"
 
   bottle do
@@ -13,12 +12,15 @@ class JdnssecTools < Formula
     sha256 "c12eafadb12264e88ef14fe4e93cdb41f0afccbb24b8cff892e8747d8ad2d73b" => :high_sierra
   end
 
-  depends_on "openjdk"
+  depends_on "ant" => :build
+  depends_on :java => "1.8"
 
   def install
+    ENV["JAVA_HOME"] = Language::Java.java_home("1.8")
+    system "ant"
     inreplace Dir["bin/*"], /basedir=.*/, "basedir=#{libexec}"
     bin.install Dir["bin/*"]
-    bin.env_script_all_files libexec/"bin", :JAVA_HOME => Formula["openjdk"].opt_prefix
+    bin.env_script_all_files libexec/"bin", Language::Java.java_home_env("1.8")
     (libexec/"lib").install Dir["lib/*"]
   end
 
