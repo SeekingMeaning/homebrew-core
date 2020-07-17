9d0695c6d662a8f53825a16e5191323c2d5248b0
Seeker <meaningseeking@protonmail.com>
2020-07-17 10:32:07 -0700

    vsts-cli: simplify files.pythonhosted.org URL

--- Formula/vsts-cli.rb
+++ Formula/vsts-cli.rb
@@ -3,7 +3,7 @@ class VstsCli < Formula
 
   desc "Manage and work with VSTS/TFS resources from the command-line"
   homepage "https://docs.microsoft.com/en-us/cli/vsts"
-  url "https://files.pythonhosted.org/packages/f9/c2/3ed698480ab30d2807fc961eef152099589aeaec3f1407945a4e07275de5/vsts-cli-0.1.4.tar.gz"
+  url "https://files.pythonhosted.org/packages/source/v/vsts-cli/vsts-cli-0.1.4.tar.gz"
   sha256 "27defe1d8aaa1fcbc3517274c0fdbd42b5ebe2c1c40edfc133d98fe4bb7114de"
   revision 2
 
