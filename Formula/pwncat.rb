8ade7e2fe85d7f7a4ec02c09fa784dfb0fba1c56
Seeker <meaningseeking@protonmail.com>
2020-07-17 10:32:04 -0700

    pwncat: simplify files.pythonhosted.org URL

--- Formula/pwncat.rb
+++ Formula/pwncat.rb
@@ -3,7 +3,7 @@ class Pwncat < Formula
 
   desc "Netcat with FW/IDS/IPS evasion, self-inject-, bind- and reverse shell"
   homepage "https://pwncat.org"
-  url "https://files.pythonhosted.org/packages/31/8a/efc45ecc5e91afc76de9c56f89de99af01d575529fead6ee24331a3fddf2/pwncat-0.1.0.tar.gz"
+  url "https://files.pythonhosted.org/packages/source/p/pwncat/pwncat-0.1.0.tar.gz"
   sha256 "4f711c3d0f22650e20ad1429a7f0c9116b930be04435e7f690746ca0e1c5cd69"
 
   bottle do
