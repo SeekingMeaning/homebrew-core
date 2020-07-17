83a90c549f7200f0756f96679b2c70562d0e7f20
Seeker <meaningseeking@protonmail.com>
2020-07-17 10:31:59 -0700

    gandi.cli: simplify files.pythonhosted.org URL

--- Formula/gandi.cli.rb
+++ Formula/gandi.cli.rb
@@ -3,7 +3,7 @@ class GandiCli < Formula
 
   desc "Command-line interface to Gandi.net products using the public API"
   homepage "https://cli.gandi.net/"
-  url "https://files.pythonhosted.org/packages/75/6d/ff2097a1b8f0142034394ca6832abade773a36efa0916500522b7d190264/gandi.cli-1.5.tar.gz"
+  url "https://files.pythonhosted.org/packages/source/g/gandi.cli/gandi.cli-1.5.tar.gz"
   sha256 "a575be04fd373d4798ae16f6cbe03e8ed16255043788fb3de13bebfe7e621c84"
   revision 2
 
