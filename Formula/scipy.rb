a90b1b1a4d860ab476457ef93af0e9ae4aa6865f
Seeker <meaningseeking@protonmail.com>
2020-07-17 10:32:05 -0700

    scipy: simplify files.pythonhosted.org URL

--- Formula/scipy.rb
+++ Formula/scipy.rb
@@ -1,7 +1,7 @@
 class Scipy < Formula
   desc "Software for mathematics, science, and engineering"
   homepage "https://www.scipy.org"
-  url "https://files.pythonhosted.org/packages/8a/6c/7777c60626cf620ce24d6349af69f3d2a4f298729d688cc4cd9528ae3c61/scipy-1.5.1.tar.gz"
+  url "https://files.pythonhosted.org/packages/source/s/scipy/scipy-1.5.1.tar.gz"
   sha256 "039572f0ca9578a466683558c5bf1e65d442860ec6e13307d528749cfe6d07b8"
   license "BSD-3-Clause"
   head "https://github.com/scipy/scipy.git"
