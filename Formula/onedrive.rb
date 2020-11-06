6a152cb49b6707eda8a42b1ecfe6e4c0010ba6dd
Seeker <meaningseeking@protonmail.com>
2020-11-06 11:01:56 -0800

    Create onedrive.rb

---
+++ Formula/onedrive.rb
@@ -0,0 +1,25 @@
+class Onedrive < Formula
+  desc "Folder synchronization with OneDrive"
+  homepage "https://github.com/abraunegg/onedrive"
+  url "https://github.com/abraunegg/onedrive/archive/v2.4.5.tar.gz"
+  sha256 "1f1f5e1f2f37376b6d96bda2426552a94a8b195f545b4fb7f3668c4fe2e8f6a0"
+  license "GPL-3.0-only"
+
+  depends_on "dmd" => :build
+  depends_on "pkg-config" => :build
+  depends_on "curl-openssl"
+  depends_on :linux
+  depends_on "sqlite"
+
+  def install
+    ENV["DC"] = "dmd"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+    bash_completion.install "contrib/completions/complete.bash"
+    zsh_completion.install "contrib/completions/complete.zsh" => "_onedrive"
+  end
+
+  test do
+    system "#{bin}/onedrive", "--version"
+  end
+end
