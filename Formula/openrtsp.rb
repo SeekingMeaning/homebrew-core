9b7b57ef2fc01c01d3ebe34219b31b80d0891bca
Seeker <meaningseeking@protonmail.com>
2021-01-19 16:31:38 -0800

    openrtsp 2021.01.18

--- Formula/openrtsp.rb
+++ Formula/openrtsp.rb
@@ -1,10 +1,10 @@
 class Openrtsp < Formula
   desc "Command-line RTSP client"
   homepage "http://www.live555.com/openRTSP"
-  url "http://www.live555.com/liveMedia/public/live.2020.11.05.tar.gz"
-  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2020.11.05.tar.gz"
+  url "http://www.live555.com/liveMedia/public/live.2021.01.18.tar.gz"
+  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2021.01.18.tar.gz"
   # Keep a mirror as upstream tarballs are removed after each version
-  sha256 "89bdfba7fd215e16be2c9d46a797bf85c5f7f7c46b53dc8af2d1171a658da5b7"
+  sha256 "1e3026cea0613779e1a0b66fb2d4fdf57bdf9c9182a3a196fa2a8e45cd8aec14"
   license "LGPL-3.0-or-later"
 
   livecheck do
