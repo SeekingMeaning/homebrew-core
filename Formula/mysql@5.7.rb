1ee6a9e13537c2a4d80f39030d6439019a8601d9
Seeker <meaningseeking@protonmail.com>
2021-01-19 16:30:30 -0800

    mysql@5.7 5.7.33

--- Formula/mysql@5.7.rb
+++ Formula/mysql@5.7.rb
@@ -1,8 +1,8 @@
 class MysqlAT57 < Formula
   desc "Open source relational database management system"
   homepage "https://dev.mysql.com/doc/refman/5.7/en/"
-  url "https://cdn.mysql.com/Downloads/MySQL-5.7/mysql-boost-5.7.32.tar.gz"
-  sha256 "9a8a04a2b0116ccff9a8d8aace07aaeaacf47329b701c5dfa9fa4351d3f1933b"
+  url "https://cdn.mysql.com/Downloads/MySQL-5.7/mysql-boost-5.7.33.tar.gz"
+  sha256 "cfcaf6f37a055d808fe1472eb0359864e2227aa0206c55c4e1961bab2ecc304b"
   license "GPL-2.0-only"
 
   livecheck do
