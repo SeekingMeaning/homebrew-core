6ada211f149fc792e7c75e13ecafa3930b853d01
Seeker <meaningseeking@protonmail.com>
2021-02-23 10:05:39 -0800

    Test

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -4,6 +4,7 @@ class Fastlane < Formula
   url "https://github.com/fastlane/fastlane/archive/2.175.0.tar.gz"
   sha256 "72ca303e7423caaa99df3aecfb841b9f15a74af8547f2aad9c047870458aa590"
   license "MIT"
+  revision 1
   head "https://github.com/fastlane/fastlane.git"
 
   livecheck do
